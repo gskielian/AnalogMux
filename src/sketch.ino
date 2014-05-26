649f7720ed0e72e254eca163e0e840dc6a6fdfd2
gskielian <gregory.kielian@gmail.com>
2014-05-25 17:56:32 -0700

    almost there, has array output

--- src/sketch.ino
+++ src/sketch.ino
@@ -2,12 +2,18 @@
 #define s1 9
 #define s2 10
 
+#define e0 6
+#define e1 5
+
+#define arrayLength 16
+
 int r0=0;
 int r1=0;
 int r2=0;
 
 int count=0;
 
+int sensorOutput[arrayLength] = { 0 }; // all elements init to 0
 
 void setup()
 {
@@ -24,7 +30,37 @@ void setup()
 
 void loop()
 {
-  int i=0;
+readSensors(sensorOutput, 0);
+readSensors(sensorOutput, 1);
+printResults(sensorOutput);
+}
+
+void printResults( int * arrayPointer) {
+//output will look like [0, 140, 24, 250, ... ,249 ]
+  int i;
+  Serial.print("[" );
+
+  for(i=0; i < arrayLength; i++ ){
+    Serial.print(*(arrayPointer + i));
+    Serial.print(",");
+  }
+
+  Serial.println("]");
+}
+
+
+void readSensors( int* arrayPointer, int enableBit) {
+  if (enableBit == 0) {
+    digitalWrite(e0,1);
+    digitalWrite(e1,0);
+  } else {
+    digitalWrite(e0,0);
+    digitalWrite(e1,1);
+    arrayPointer += 8;
+  }
+
+
+  int i;
   for ( i = 0; i <= 7 ; i++ ) {
     r0=( count & 0x01);
     r1=( (count>>1) & 0x01);
@@ -37,9 +73,11 @@ void loop()
     Serial.print("Reading on ");
     Serial.print(count);
     Serial.print(" is ");
-    Serial.println(analogRead(A0));
+    *arrayPointer = analogRead(A0);
+    Serial.println(*arrayPointer);
 
     (count >=7) ? count=0 : count++;
-    delay(500);
+    delay(100);
   }
+
 }
